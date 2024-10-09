29cd24a33fe0652dff6a11ff3a2333431c2906d7
Aditya Kalambe <adityakalambe20@gmail.com>
2024-10-09 21:34:07 +0530

    final commit

---
+++ src/app/Utility.ts
@@ -0,0 +1,10 @@
+export class Lists{
+    static states: string[] = [
+        "Maharastra",
+        "Delhi",
+        "Assam",
+        "Goa",
+        "MP",
+        "UP"
+    ];
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,58 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Lists } from './Utility';
+import { CommonModule, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'TemplateForm';
+
+  states:string[] = Lists.states;
+
+  student:any = {
+    firstName: '',
+    lastName: '',
+    username: '',
+    gender: '',
+    city: '',
+    state: '',
+    email: '',
+    password: ''
+  }
+
+  studentDataFromForm:any;
+  submit():void{
+    this.studentDataFromForm = this.student;
+  }
+
+  reset():void{
+    this.studentDataFromForm = {
+      firstName: '',
+      lastName: '',
+      username: '',
+      gender: '',
+      city: '',
+      state: '',
+      email: '',
+      password: ''
+    };
+
+    this.student = {
+      firstName: '',
+      lastName: '',
+      username: '',
+      gender: '',
+      city: '',
+      state: '',
+      email: '',
+      password: ''
+    };
+  }
 }
